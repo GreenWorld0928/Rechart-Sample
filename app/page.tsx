71c39e45b800813bf92bfbe580e98e5e87e41ccd
GreenWorld928 <green.world928@outlook.com>
2024-03-20 20:10:24 -0500

    add badges for each item

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { useState } from "react";
 import {
   Area,
-  AreaChart,
   CartesianGrid,
   ComposedChart,
   Line,
@@ -21,6 +19,8 @@ export default function Home() {
     percentile50?: number;
     percentile75?: number;
     percentile25?: number;
+    percentileMin?: number;
+    percentileMax?: number;
   }
   const subtractIfDefined = (a: number | undefined, b: number | undefined): number | undefined => {
     return (a !== undefined && b !== undefined) ? a - b : undefined;
@@ -242,14 +242,16 @@ export default function Home() {
     percentile50: item.percentile50,
     percentile75: subtractIfDefined(item.percentile75, item.percentile25),
     percentile25: subtractIfDefined(item.percentile25, item.percentile5),
+    percentileMin: 70,
+    percentileMax: 180,
   }));
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <ResponsiveContainer width="100%" height={200}>
+      <ResponsiveContainer width="90%" height={400}>
         <ComposedChart
           width={500}
-          height={200}
+          height={400}
           data={Newdata}
           margin={{
             top: 10,
@@ -257,11 +259,10 @@ export default function Home() {
             left: 30,
             bottom: 0,
           }}
-          style={{backgroundColor: "green"}}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" style={{fontSize:10}}/>
-          <YAxis unit="mg/dL" style={{fontSize:10}}/>
+          <XAxis dataKey="name" style={{ fontSize: 10 }} />
+          <YAxis unit="mg/dL" style={{ fontSize: 10 }} />
           {/* <Tooltip /> */}
           <Area
             type="monotone"
@@ -297,8 +298,52 @@ export default function Home() {
             dataKey="percentile50"
             stroke="#2e5481"
             dot={false}
+            strokeWidth={2}
+          />
+          <Line
+            type="monotone"
+            stroke="#65a467"
+            dataKey="percentileMax"
+            dot={false}
+            strokeWidth={3}
+          />
+          <Line
+            type="monotone"
+            dataKey="percentileMin"
+            stroke="#65a467"
+            dot={false}
+            strokeWidth={3}
           />
         </ComposedChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="10%" height={400} className="flex items-center">
+        <div className="flex flex-col justify-center">
+        <div className="flex my-1">
+            <div className="w-10 h-5 border rounded-md mr-3 percent95-color"></div>
+            <div className="text-sm text-center items-center">75-95%</div>
+          </div>
+          <div className="flex my-1">
+            <div className="w-10 h-5 border rounded-md mr-3 percent75-color"></div>
+            <div className="text-sm text-center items-center">50-75%</div>
+          </div>
+          <div className="flex items-center  my-1">
+            <div className="w-8 h-1 border rounded-md ml-1 mr-3 percent50-color"></div>
+            <div className="text-sm ml-3 text-center items-center">50%</div>
+          </div>
+          <div className="flex my-1">
+            <div className="w-10 h-5 border rounded-md mr-3 percent75-color"></div>
+            <div className="text-sm text-center items-center">25-50%</div>
+          </div>
+
+          <div className="flex my-1">
+            <div className="w-10 h-5 border rounded-md mr-3 percent25-color"></div>
+            <div className="text-sm text-center items-center">5-25%</div>
+          </div>
+          <div className="flex items-center  my-5">
+            <div className="w-10 h-2 border rounded-md mr-1 target-range-color"></div>
+            <div className="text-sm ml-1 text-center items-center">Target Range<br></br> 70-180 mg/dL</div>
+          </div>
+        </div>
       </ResponsiveContainer>
     </div>
   );
